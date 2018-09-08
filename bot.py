d8296efcd58468342ee68bb557e17585ec99ad75
Daria Primorskaya <riokko@rizn.org>
2018-09-08 18:15:50 +0300

    Бот для телеграма

---
+++ bot.py
@@ -0,0 +1,36 @@
+# Импортируем нужные компоненты
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Настройки прокси
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+import logging
+logging.basicConfig(format = '%(name)s - %(levelname)s - %(message)s',
+                    level = logging.INFO,
+                    filename = 'bot.log'
+                    )
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+# функция, которая соединяется с платформой Telegram, "тело" нашего бота
+def main():
+    mybot = Updater("646613110:AAEaqvqJnNUOqVNNFTmHU5mTEVxN3VJpQlo", request_kwargs = PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle
+
+# Вызываем функцию — эта строчка, собственно, запускает бота
+main()
